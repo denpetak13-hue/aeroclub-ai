e96b0f7ac9379108d9fdf9650e349cb175b886aa
Denis <den.petak13@gmail.com>
2026-02-21 08:35:37 +0100

    fix __file__ syntax

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 # =========================
 
-BASE_DIR = Path(**file**).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent
 ENV_PATH = BASE_DIR.parent / ".env"
 
 load_dotenv(dotenv_path=ENV_PATH)
